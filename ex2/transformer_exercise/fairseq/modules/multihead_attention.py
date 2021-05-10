78f6fb850cac23851362d9a3b45f078ce87c903b
nyochai7 <naamayochai@mail.tau.ac.il>
2021-05-10 15:17:37 +0300

    Moved attn_mask to device

--- ex2/transformer_exercise/fairseq/modules/multihead_attention.py
+++ ex2/transformer_exercise/fairseq/modules/multihead_attention.py
@@ -169,6 +169,7 @@ class MultiheadAttention(nn.Module):
             for i in range(N):
                 attn_mask[i * self.num_heads + mask_head] = torch.ones((query.shape[0], key.shape[0]), dtype=torch.bool)
             print('attn_mask.shape=', attn_mask.shape)
+            attn_mask = attn_mask.to(key.device)
 
         if (
                 not self.enable_fairseq_version

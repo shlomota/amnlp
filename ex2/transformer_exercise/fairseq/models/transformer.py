51a5a3de7f1d99d4df404111946404dbd784277d
shlomota <shlomotannor@mail.tau.ac.il>
2021-05-11 16:48:22 +0300

    added prints

--- ex2/transformer_exercise/fairseq/models/transformer.py
+++ ex2/transformer_exercise/fairseq/models/transformer.py
@@ -438,6 +438,7 @@ class TransformerEncoder(FairseqEncoder):
         if args.enc_layer_configuration:
             print(args.enc_layer_configuration)
             print(index)
+            print(args.encoder_layers)
             layer = TransformerEncoderLayer(args, args.enc_layer_configuration[index])
         else:
             layer = TransformerEncoderLayer(args)
